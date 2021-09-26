73824c3ce3b8da62a19aa04c0b5d7750d16b0b0e
FoxTimew <foxtimew@hotmail.com>
2021-09-26 20:11:43 +0200

    Curves & Push sys added and functional

--- Derive/Assets/FoxT_Ressources/Scripts/PC/Controller.cs
+++ Derive/Assets/FoxT_Ressources/Scripts/PC/Controller.cs
@@ -1,21 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fox.Editor;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Controller : MonoBehaviour
 {
-    private Player pc;
-
+    public Player pc;
 	//Inspector Elements
 	//Debug
 	public bool debug;
 	public float minAccelerationStep = 1, maxAccelerationStep = 10;
 	public float minDecelerationStep = 1, maxDecelerationStep = 10;
-	
-	//Push elements
-	public sbyte currentPushMethod = 0, lastPushMethod = 0;
-	public int howManyPart, lastPartNumber;
 
 	//Script Elements
 	//Movement elements
@@ -24,13 +20,7 @@ public class Controller : MonoBehaviour
 	public float decelerationStep;
 
 	//Push
-	public float forceMultiplicator;
-	public bool curveUsed;
-	//Simplified & Advanced methods
-	public float[] pushDelay;
-	public float[] pushDecelerationStep;
-	//Curve method
-	public AnimationCurve pushCurve;
+	public CurveOptions curves = new CurveOptions();
 
 	private Vector2 joystick;
 	private void Awake()
@@ -50,7 +40,12 @@ public class Controller : MonoBehaviour
 
 	private void FixedUpdate()
 	{
-		pc.Move(joystick);
+		if (curves.isPlaying)
+		{
+			Debug.Log(curves.velocity);
+			GetComponent<Rigidbody>().velocity = curves.velocity;
+		}
+		else pc.Move(joystick);
 	}
 
 	private void InputHandler()
@@ -68,45 +63,6 @@ public class Controller : MonoBehaviour
 
 	public void Push(Vector3 direction, float force)
 	{
-		StartCoroutine(PushUpdate(direction, force));
-	}
-
-	private IEnumerator PushUpdate(Vector3 dir, float force)
-	{
-		pc.isPushing = true;
-		float timeElapsed = 0;
-		if (curveUsed)
-		{
-			while (true)
-			{
-				GetComponent<Rigidbody>().velocity = -dir * pushCurve.Evaluate(timeElapsed) * force * forceMultiplicator;
-				if (timeElapsed >= pushCurve.keys[pushCurve.keys.Length - 1].time) break;
-				yield return new WaitForEndOfFrame();
-				timeElapsed += Time.deltaTime;
-			}
-		}
-		else
-		{
-			for (int i = 0; i < pushDelay.Length; i++)
-			{
-				while (true)
-				{
-					GetComponent<Rigidbody>().velocity = -dir * force * forceMultiplicator;
-					try
-					{
-						force = Mathf.Clamp(force - (pushDecelerationStep[i] / pushDelay[i]), 0, force);
-					}
-					catch
-					{
-						Debug.LogError("On controller, about Push fonctions, Delay can't be equal to 0");
-						break;
-					}
-					if (timeElapsed >= pushDelay[i]) break;
-					yield return new WaitForEndOfFrame();
-					timeElapsed += Time.deltaTime;
-				}
-			}
-		}
-		pc.isPushing = false;
+		StartCoroutine(curves.Play(-direction, force));
 	}
 }
--- Derive/Assets/FoxT_Ressources/Scripts/Utils/Editor/Playable_Controller/ControllerEditor.cs
+++ Derive/Assets/FoxT_Ressources/Scripts/Utils/Editor/Playable_Controller/ControllerEditor.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Fox.Editor;
 
 [CustomEditor(typeof(Controller))]
 public class ControllerEditor : Editor
 {
+	int test;
 	public override void OnInspectorGUI()
 	{
 		Controller ctrl = (Controller)target;
@@ -39,72 +41,6 @@ public class ControllerEditor : Editor
 		GUI.color = Color.white;
 		GUILayout.Label("Methods");
 		GUILayout.Label("");
-		GUILayout.Label("");
-		GUILayout.Label("");
-		sbyte debugDelay = 0;
-		if (ctrl.debug)
-		{
-			debugDelay = 80;
-		}
-		if (GUI.Button(new Rect(5, 200 + debugDelay, Screen.width / 3 - 10, 25), "Simplified"))
-		{
-			ctrl.currentPushMethod = 0;
-		}
-		else if (GUI.Button(new Rect(5 + (Screen.width / 3), 200 + debugDelay, Screen.width / 3 - 10, 25), "Advanced"))
-		{
-			ctrl.currentPushMethod = 1;
-		}
-		else if (GUI.Button(new Rect(5 + (Screen.width / 3) * 2, 200 + debugDelay, Screen.width / 3 - 10, 25), "Curve"))
-		{
-			ctrl.currentPushMethod = 2;
-		}
-		GUILayout.Label("");
-		ctrl.forceMultiplicator = EditorGUILayout.FloatField("Force Multiplicator", ctrl.forceMultiplicator);
-		GUILayout.Label("");
-		switch (ctrl.currentPushMethod)
-		{
-			case 1:
-				ctrl.curveUsed = false;
-				ctrl.howManyPart = EditorGUILayout.IntField("Part", ctrl.howManyPart);
-				if (ctrl.howManyPart < 0)
-				{
-					ctrl.howManyPart = 0;
-				}
-				if (ctrl.lastPartNumber != ctrl.howManyPart)
-				{
-					ctrl.pushDelay = new float[ctrl.howManyPart];
-					ctrl.pushDecelerationStep = new float[ctrl.howManyPart];
-				}
-				else ctrl.lastPartNumber = ctrl.howManyPart;
-				GUILayout.Label("");
-				for (int i = 0; i < ctrl.pushDelay.Length; i++)
-				{
-					GUILayout.Label("Part " + (i + 1));
-					ctrl.pushDelay[i] = EditorGUILayout.FloatField("Delay", ctrl.pushDelay[i]);
-					ctrl.pushDecelerationStep[i] = EditorGUILayout.FloatField("Deceleration Step", ctrl.pushDecelerationStep[i]);
-					GUILayout.Label("");
-				}
-				break;
-			case 2:
-				ctrl.curveUsed = true;
-				ctrl.pushCurve = EditorGUILayout.CurveField("Curve", ctrl.pushCurve);
-				break;
-			default:
-				ctrl.curveUsed = false;
-				if (ctrl.lastPushMethod != ctrl.currentPushMethod)
-				{
-					ctrl.pushDelay = new float[2];
-					ctrl.pushDecelerationStep = new float[2];
-				}
-				else ctrl.lastPushMethod = ctrl.currentPushMethod;
-				GUILayout.Label("Part 1");
-				ctrl.pushDelay[0] = EditorGUILayout.FloatField("Delay", ctrl.pushDelay[0]);
-				ctrl.pushDecelerationStep[0] = EditorGUILayout.FloatField("Deceleration Step", ctrl.pushDecelerationStep[0]);
-				GUILayout.Label("");
-				GUILayout.Label("Part 2");
-				ctrl.pushDelay[1] = EditorGUILayout.FloatField("Delay", ctrl.pushDelay[1]);
-				ctrl.pushDecelerationStep[1] = EditorGUILayout.FloatField("Deceleration Step", ctrl.pushDecelerationStep[1]);
-				break;
-		}
+		CurveOptionsEditor.CurveFields(ctrl.curves);
 	}
 }
