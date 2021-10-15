6fdb4ab6bb5fab54a75dd66f9a85386f1cb84321
Lo <lo.accard@gmail.com>
2021-10-15 17:18:59 +0200

    Start visual

--- Derive/Assets/LAC_Ressources/LAC_Script/Interactible/Canon/CanonBehaviour.cs
+++ Derive/Assets/LAC_Ressources/LAC_Script/Interactible/Canon/CanonBehaviour.cs
@@ -7,15 +7,19 @@ public class CanonBehaviour : MonoBehaviour
     public GameObject bulletPrefab;
 
     [Range(0,360)]
-    public float shootAngle; 
+    public float shootAngle;
+    [Range(0,5)]
+    public float shootDist;
+    public float zOffset;
     public float shootInitialSpeed;
+    
     
 
     [Header("Load")]
 
     public List<BulletBehaviour> bulletLoaded;
     public Vector3 loadPos;
-    [Range(0.1f,2)]
+    [Range(0.1f,5)]
     public float loadDetectRadius;
     GameObject loadObject;
     LoadPoint loadPoint;
@@ -23,8 +27,9 @@ public class CanonBehaviour : MonoBehaviour
     private void Awake()
     {
         loadObject = new GameObject("LoadDetector");
+        //loadObject.transform.position = transform.position;
         loadObject.transform.parent = transform;
-        loadObject.transform.position = loadPos;
+        loadObject.transform.localPosition = loadPos;
         loadPoint =(LoadPoint)loadObject.AddComponent(typeof(LoadPoint));
 
         loadPoint.canon = this;
@@ -40,7 +45,7 @@ public class CanonBehaviour : MonoBehaviour
     {
         if(bulletLoaded.Count > 0)
         {
-            Vector3 shootPos = new Vector3(Mathf.Cos(shootAngle * Mathf.Deg2Rad), Mathf.Sin(shootAngle * Mathf.Deg2Rad), 0);
+            Vector3 shootPos = new Vector3(Mathf.Cos(shootAngle * Mathf.Deg2Rad) * shootDist, Mathf.Sin(shootAngle * Mathf.Deg2Rad) * shootDist, zOffset);
             Vector2 shootDir = new Vector2(Mathf.Cos(shootAngle * Mathf.Deg2Rad), Mathf.Sin(shootAngle * Mathf.Deg2Rad));
             bulletLoaded[bulletLoaded.Count - 1].Shoot(shootPos + transform.position, shootDir, shootInitialSpeed);
             bulletLoaded.Remove(bulletLoaded[bulletLoaded.Count - 1]);
@@ -62,8 +67,8 @@ public class CanonBehaviour : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position  + new Vector3(Mathf.Cos(shootAngle * Mathf.Deg2Rad)  , Mathf.Sin(shootAngle * Mathf.Deg2Rad ),0), 0.2f);
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(transform.position + new Vector3(Mathf.Cos(shootAngle * Mathf.Deg2Rad) * shootDist, Mathf.Sin(shootAngle * Mathf.Deg2Rad) * shootDist, zOffset), 0.5f);
 
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere((loadObject)?loadObject.transform.position : transform.position + loadPos, loadDetectRadius);
